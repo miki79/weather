883cba8fbe83e6cd50558bf4ccdf2e9c2c55180e
Michele Giardi <miki79@users.noreply.github.com>
2018-04-17 13:39:55 +0100

    Add x-ray and cache

--- src/ip.js
+++ src/ip.js
@@ -1,15 +1,39 @@
-const request = require('request');
+const AWSXRay = require('aws-xray-sdk');
+const http = AWSXRay.captureHTTPs(require('http'));
+const cacheManager = require('cache-manager');
+const fsStore = require('cache-manager-fs');
+const diskCache = cacheManager.caching({
+  store: fsStore,
+  options: {
+    ttl: 86400,
+    maxsize: 1000 * 1000 * 1000 /* max size in bytes on disk */,
+    path: '/tmp',
+  },
+});
 
 const getCoordinates = ip => {
-  return new Promise((resolve, reject) => {
-    const url = `http://ip-api.com/json/${ip}`;
-    request({ url, json: true }, (err, response, body) => {
-      if (err) {
-        return reject(err);
-      } else if (body.lat && body.lon) {
-        return resolve({ lat: body.lat, lon: body.lon });
-      }
-      return reject(new Error('Coordinates not found'));
+  return diskCache.wrap(ip, () => {
+    return new Promise((resolve, reject) => {
+      const url = `http://ip-api.com/json/${ip}`;
+
+      http.get(url, res => {
+        res.setEncoding('utf8');
+        let body = '';
+        res.on('data', data => {
+          body += data;
+        });
+        res.on('end', () => {
+          console.info(body);
+          body = JSON.parse(body);
+          if (body.lat && body.lon) {
+            return resolve({ lat: body.lat, lon: body.lon });
+          }
+          return reject(new Error('Coordinates not found'));
+        });
+        res.on('error', () => {
+          reject(new Error('Error HTTP'));
+        });
+      });
     });
   });
 };
--- src/weather.js
+++ src/weather.js
@@ -1,28 +1,38 @@
-const request = require('request');
+const AWSXRay = require('aws-xray-sdk');
+const https = AWSXRay.captureHTTPs(require('https'));
 const APP_KEY = process.env.APP_WEATHER_KEY;
 
 const getWeather = coordinates => {
-  console.info(APP_KEY);
   return new Promise((resolve, reject) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${
       coordinates.lat
     }&lon=${coordinates.lon}&units=metric&APPID=${APP_KEY}`;
-    request({ url, json: true }, (err, response, body) => {
-      if (err) {
-        return reject(err);
-      } else if (response.statusCode !== 200) {
-        return reject(new Error(body.message));
-      }
 
-      const weather = [];
-      body.weather.forEach(data => {
-        weather.push({
-          main: data.main,
-          description: data.description,
-          icon: `https://openweathermap.org/img/w/${data.icon}.png`,
-        });
+    https.get(url, res => {
+      res.setEncoding('utf8');
+      let body = '';
+      res.on('data', data => {
+        body += data;
       });
-      return resolve(weather);
+      res.on('end', () => {
+        console.info(body);
+        body = JSON.parse(body);
+        if (body.weather) {
+          const weather = [];
+          body.weather.forEach(data => {
+            weather.push({
+              main: data.main,
+              description: data.description,
+              icon: `https://openweathermap.org/img/w/${data.icon}.png`,
+            });
+          });
+          return resolve(weather);
+        }
+        return reject(new Error('Error fetch weather'));
+      });
+      res.on('error', () => {
+        reject(new Error('Error HTTP'));
+      });
     });
   });
 };
